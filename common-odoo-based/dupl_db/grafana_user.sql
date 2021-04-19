c2a7f08e132dcde027090806e6ceb28f8f4615f6
KolushovAlexandr <alexandr.frrost@gmail.com>
2021-04-19 16:35:46 +0500

    [ADD] grafana user creating instructions

---
+++ common-odoo-based/dupl_db/grafana_user.sql
@@ -0,0 +1,15 @@
+CREATE ROLE grafana LOGIN PASSWORD 'grafana';
+GRANT CONNECT ON database fsa_parser TO grafana;
+GRANT USAGE ON SCHEMA public TO grafana;
+SELECT 'GRANT SELECT ON '||schemaname||'."'||tablename||'" TO grafana;' FROM pg_tables WHERE schemaname IN ('public') and tablename like 'fsa_%' ORDER BY schemaname, tablename;
+
+ GRANT SELECT ON public."fsa_parser_certificate" TO grafana;
+ GRANT SELECT ON public."fsa_parser_certificate_list" TO grafana;
+ GRANT SELECT ON public."fsa_parser_declaration" TO grafana;
+ GRANT SELECT ON public."fsa_parser_declaration_list" TO grafana;
+ GRANT SELECT ON public."fsa_parser_ral" TO grafana;
+ GRANT SELECT ON public."fsa_parser_ral_list" TO grafana;
+ GRANT SELECT ON public."fsa_parser_wizard_main" TO grafana;
+ GRANT SELECT ON public."fsa_parser_wizard_main_sql" TO grafana;
+ GRANT SELECT ON public."fsa_parser_wizard_main_subline" TO grafana;
+ 
